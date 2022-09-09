5696dbfd5851da8e3a8e918cc22c737058d67e2f
Vladimir Pankov <vdrpkv@gmail.com>
2022-09-09 16:24:19 +0300

    Lowecase api key and prefix shortcuts

--- cmd/cli/cointool/cointool.go
+++ cmd/cli/cointool/cointool.go
@@ -16,13 +16,13 @@ var (
 func init() {
 	Command.PersistentFlags().StringVarP(
 		&variables.ApiKey,
-		"api-key", "K", "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
+		"api-key", "k", "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
 		"coinmarketcap API key",
 	)
 
 	Command.PersistentFlags().StringVarP(
 		&variables.ApiPrefix,
-		"api-prefix", "P", "sandbox",
+		"api-prefix", "p", "sandbox",
 		"coinmarketcap API prefix: sandbox or pro",
 	)
 

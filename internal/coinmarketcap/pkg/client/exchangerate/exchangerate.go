d56a72aac49081c7737527479d7f2d69ccfa4fc1
Vladimir Pankov <vdrpkv@gmail.com>
2022-09-09 16:24:20 +0300

    Add coinmarketcap client for exchange rate

---
+++ internal/coinmarketcap/pkg/client/exchangerate/exchangerate.go
@@ -0,0 +1,69 @@
+package exchangerate
+
+import (
+	"context"
+	"fmt"
+
+	exchangeRateClient "github.com/vdrpkv/cointool/internal/client/currency/exchangerate"
+
+	coinmarketcapExchangeRate "github.com/vdrpkv/cointool/internal/coinmarketcap/internal/exchangerate"
+	coinmarketcapFiatRecognizer "github.com/vdrpkv/cointool/internal/coinmarketcap/internal/fiat"
+
+	"github.com/vdrpkv/cointool/internal/currency"
+)
+
+type client struct {
+	apiKey, apiPrefix string
+}
+
+var _ exchangeRateClient.ExchangeRateGetter = (*client)(nil)
+
+func NewExchangeRateGetter(
+	apiKey, apiPrefix string,
+) exchangeRateClient.ExchangeRateGetter {
+	return &client{
+		apiKey:    apiKey,
+		apiPrefix: apiPrefix,
+	}
+}
+
+func (c *client) GetExchangeRate(
+	ctx context.Context,
+	from, to currency.Symbol,
+) (
+	currency.ExchangeRate,
+	error,
+) {
+	// Check is FROM currency fiat one.
+	isFiat, err := coinmarketcapFiatRecognizer.Get(
+		ctx,
+		c.apiKey, c.apiPrefix,
+		from,
+	)
+
+	if err != nil {
+		return 0, fmt.Errorf("recognize fiat currency: %w", err)
+	}
+
+	// Flip symbols because coinmarketcapExchangeRate.Get
+	// does not find exchange rate from fiat currency.
+	if isFiat {
+		from, to = to, from
+	}
+
+	rate, err := coinmarketcapExchangeRate.Get(
+		ctx,
+		c.apiKey, c.apiPrefix,
+		from, to,
+	)
+	if err != nil {
+		return 0, fmt.Errorf("get exchange rate: %w", err)
+	}
+
+	// Flip exchange rate if first currency is fiat one.
+	if isFiat {
+		rate = rate.Flip()
+	}
+
+	return rate, nil
+}

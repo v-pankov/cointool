15126aa92650d75c8bf4492096172eccc495a34f
Vladimir Pankov <vdrpkv@gmail.com>
2022-09-09 16:24:20 +0300

    Add rate command handler tests

--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/vdrpkv/cointool
 
 go 1.18
 
-require github.com/spf13/cobra v1.5.0
+require (
+	github.com/spf13/cobra v1.5.0
+	github.com/stretchr/testify v1.8.0
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/handler/rate/rate_test.go
@@ -0,0 +1,87 @@
+package rate
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vdrpkv/cointool/internal/currency"
+	"github.com/vdrpkv/cointool/internal/mocks"
+)
+
+func Test_RateCommandHandler(t *testing.T) {
+	var (
+		stubCtx  = context.Background()
+		stubFrom = currency.Symbol("STUB_FROM")
+		stubTo   = currency.Symbol("STUB_TO")
+		stubErr  = errors.New("stub error")
+	)
+
+	type (
+		testCaseGiveStubs struct {
+			rate currency.ExchangeRate
+			err  error
+		}
+
+		testCaseGive struct {
+			stubs testCaseGiveStubs
+		}
+
+		testCaseWant struct {
+			rate currency.ExchangeRate
+			err  error
+		}
+
+		testCase struct {
+			name string
+			give testCaseGive
+			want testCaseWant
+		}
+	)
+
+	for _, testCase := range []testCase{
+		{
+			"error",
+			testCaseGive{
+				stubs: testCaseGiveStubs{
+					err: stubErr,
+				},
+			},
+			testCaseWant{
+				err: stubErr,
+			},
+		},
+		{
+			"no error",
+			testCaseGive{
+				stubs: testCaseGiveStubs{
+					rate: 0.99,
+				},
+			},
+			testCaseWant{
+				rate: 0.99,
+			},
+		},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			exchangeRateGetterMock := mocks.NewExchangeRateGetter(t)
+
+			exchangeRateGetterMock.
+				On(
+					"GetExchangeRate", stubCtx, stubFrom, stubTo,
+				).
+				Return(
+					testCase.want.rate, testCase.want.err,
+				)
+
+			rateCommandHandler := New(exchangeRateGetterMock)
+			gotRate, gotErr := rateCommandHandler.HandleRateCommand(
+				stubCtx, stubFrom, stubTo,
+			)
+
+			require.ErrorIs(t, gotErr, testCase.want.err)
+			require.Equal(t, testCase.want.rate, gotRate)
+		})
+	}
+}
